92d58b2871618d4ab394e3737922716bba6d4294
Wandalrgalvan <95222468+Wandalrgalvan@users.noreply.github.com>
2026-02-16 00:16:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def consultar_ia(prompt):
         return "Error: No hay API Key configurada."
     try:
         genai.configure(api_key=api_key)
-        model = genai.GenerativeModel('gemini-1.5-flash')
+        model = genai.GenerativeModel('gemini-pro')
         response = model.generate_content(prompt)
         return response.text
     except Exception as e:
